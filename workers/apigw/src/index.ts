fa2b28a1140f6cd91f131c741f1b0125061a6575
Cole MacKenzie <colemackenzie1@gmail.com>
2022-09-10 15:49:46 -0700

    apigw: add apigw worker

---
+++ workers/apigw/src/index.ts
@@ -0,0 +1,41 @@
+import { Router } from 'itty-router';
+
+export interface Env {}
+
+const router = Router();
+
+router.all('/tfstate/*', async (request: Request) => {
+  const authorization: string | undefined = request.headers.get('authorization');
+  if (!authorization) return fetch(request);
+
+  const [authScheme, token] = authorization.split(' ');
+  if (authScheme.toLowerCase() !== 'basic')
+    return new Response(`Authentication type ${authScheme} is not supported for this endpoint.`, { status: 401 });
+
+  const [user, pass] = atob(token).split(':');
+  const url = new URL(request.url);
+  url.hostname = 'tfstate.mirio.dev';
+  url.pathname = url.pathname.slice('/tfstate'.length);
+
+  const req = new Request(url.toString(), request);
+  const headers = req.headers;
+  headers.set('Cf-Access-Client-Id', user);
+  headers.set('Cf-Access-Client-Secret', pass);
+
+  return fetch(req);
+});
+
+router.all('/echo/*', async (request: Request) => {
+  const url = new URL(request.url);
+  url.host = 'echo.mirio.dev';
+  url.pathname = url.pathname.slice('/echo'.length);
+  return fetch(url.toString(), request);
+});
+
+router.all('*', () => new Response('Go away.', { status: 404 }));
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return router.handle(request, env);
+  },
+};
