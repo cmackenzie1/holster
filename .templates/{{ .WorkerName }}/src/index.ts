ab1cfcf23bf5971c67b78c0f1dd8fdf767739407
Cole MacKenzie <colemackenzie1@gmail.com>
2022-09-10 13:20:13 -0700

    add new template for workers

---
+++ .templates/{{ .WorkerName }}/src/index.ts
@@ -0,0 +1,7 @@
+export interface Env {}
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response('Hello World!');
+  },
+};
