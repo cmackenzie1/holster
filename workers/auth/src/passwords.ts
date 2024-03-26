0fe785b93b85adc1412e7ff942174a64667c8161
Cole MacKenzie <colemackenzie1@gmail.com>
2024-03-25 21:41:38 -0700

    feat: remove extraneous esbuild and jest configs
    This commits removes the esbuild and jest configs from any
    individual worker directory. This is because the configs are
    now in the root of the project and are shared across all workers.
    
    `esbuild` is now the default bundler for all workers, provided by `wrangler`.
    
    `jest` is now the default test runner for all workers, provided by `jest` (using `ts-jest`).

--- workers/auth/src/passwords.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcryptjs';
-
-bcrypt.setRandomFallback((num: number) => Array.from(crypto.getRandomValues(new Int32Array(num))));
-
-export const generateSalt = async (): Promise<string> => bcrypt.genSalt(10);
-
-export const hashPassword = async (password: string, salt: string): Promise<string> => bcrypt.hash(password, salt);
-
-export const comparePassword = async (s: string, hash: string): Promise<boolean> => bcrypt.compare(s, hash);
---
+++ workers/stocks/__tests__/hello.test.ts
@@ -0,0 +1,3 @@
+test('hello, world', () => {
+  expect(true).toBeTruthy();
+});
