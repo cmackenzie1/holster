cc00111ff254e5399ec892159795d58b3a955f78
Cole MacKenzie <colemackenzie1@gmail.com>
2024-03-25 22:00:51 -0700

    chore: remove template and cmd dirs
    No longer needed as we have a new way to generate workers.
    
    - Closes #15

--- .templates/{{ .WorkerName }}/src/index.ts
+++
@@ -1,7 +0,0 @@
-export interface Env {}
-
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response('Hello World!');
-  },
-};
