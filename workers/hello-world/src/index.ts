a57cb0e1f69e7dfe4d8705042242b279d4a59614
Cole MacKenzie <colemackenzie1@gmail.com>
2024-06-14 16:17:18 -0700

    feat(hello-world): add basic http analytics

--- workers/hello-world/src/index.ts
+++ workers/hello-world/src/index.ts
@@ -1,5 +1,20 @@
+import { HTTPAnalytics } from "analytics";
+
+interface Env {
+	HTTP_REQUESTS: AnalyticsEngineDataset;
+}
+
 export default {
-	async fetch(request: Request): Promise<Response> {
+	async fetch(
+		request: Request,
+		env: Env,
+		_: ExecutionContext,
+	): Promise<Response> {
+		// Log the request
+		if (env.HTTP_REQUESTS) {
+			new HTTPAnalytics(env.HTTP_REQUESTS).observe(request);
+		}
+
 		return new Response("Hello, World!");
 	},
 };
