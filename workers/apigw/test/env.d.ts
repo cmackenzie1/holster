da7076220edaa25b9fe5d47a81564c0271e0721f
Cole Mackenzie <colemackenzie1@gmail.com>
2026-01-26 19:34:57 -0800

    Remove tfstate and apigw workers

--- workers/apigw/test/env.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Env } from "../src/types/env";
-
-declare module "cloudflare:test" {
-	// Controls the type of `import("cloudflare:test").env`
-	interface ProvidedEnv extends Env {}
-}
--- workers/tfstate/src/types/env.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Env {
-	// tfstate storage and locking
-	TFSTATE_BUCKET: R2Bucket;
-	TFSTATE_LOCK: DurableObjectNamespace;
-}
